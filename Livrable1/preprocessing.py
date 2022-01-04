8fd1f2f9ab2a4b0f0e0cd2a55a44ff101a40de1e
Corentin DEVROUETE <corentindevrouete.pro@gmail.com>
2022-01-04 09:21:43 +0100

    CNN works

--- Livrable1/preprocessing.py
+++ Livrable1/preprocessing.py
@@ -1,35 +1,47 @@
-
-import os
-import glob
 import shutil
+import os 
+import glob 
 
+
+def preprocessing():
+    path = './Dataset'
+    types = ('*.png', '*.jpg')
+
+    #SIZE OF DATASET
+    painting_size = 3000
+    schematic_size = 3000
+    sketch_size = 3000
+    text_size = 1000
+
+    index_file = 0
+    
+    #Folder
+    painting_folder = glob.glob(path+'/Painting/*.jpg')
+    schematics_folder = glob.glob(path+'/Schematics/*.jpg')
+    sketch_folder = glob.glob(path+'/Sketch/*.jpg')
+    text_folder = glob.glob(path+'/Text/*.jpg')
+
+    #Destination
+    path = './Dataset/Other/'
+
+    for i in range(3000):
+        paint_file = str.split(painting_folder[i], '/')[-1]
+        schematics_file = str.split(schematics_folder[i], '/')[-1]
+        text_file = str.split(text_folder[i], '/')[-1]        
+
+
+        shutil.move(painting_folder[i], path+ paint_file)
+        shutil.move(schematics_folder[i], path+ schematics_file)
+        shutil.move(text_folder[i], path+ text_file)
+
+    for i in range(1000):
+        sketch_file = str.split(sketch_folder[i], '/')[-1]
+        shutil.move(sketch_folder[i], path+ sketch_file)
 
 def main():
 
-    folder_name = ['Painting',
-                   'Photo',
-                   'Schematics',
-                   'Sketch',
-                   'Text'
-                   ]
+    preprocessing()
 
 
-    for name in folder_name:
-        file_size = len(os.listdir('../Dataset/' + name + '/'))
-        folder_name = "train"
-        i = 0
-        while len(os.listdir('../Dataset/' + name + '/')) != 0:
-            if i >= int(( file_size * 80)/100):
-                folder_name = "test"
-
-            list_of_files = glob.glob('../Dataset/' + name + '/*')  # * means all if need specific format then *.csv
-            latest_file = max(list_of_files, key=os.path.getctime)
-            last_file_splitted = latest_file.split('/')
-            shutil.move(latest_file, '../Dataset/' + folder_name + '/' + name + '/'+last_file_splitted[-1])
-            print('../Dataset/' + folder_name + '/' + name + '/'+last_file_splitted[-1])
-            i+=1
-
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
